e190ae6179de528d0f99692bb12939d5400df235
Sharmaaswati <sharma2807swati@gmail.com>
2026-04-07 17:25:59 +0530

    Add prime and perfect number checks in Main.java
    Implemented logic to check if numbers are prime or perfect and output the results accordingly.

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        // TODO: Read n integers. For each number print:
+         // TODO: Read n integers. For each number print:
         //   "Prime"     if it is prime
         //   "Perfect"   if it is a perfect number (sum of proper divisors == itself, e.g. 6=1+2+3)
         //   "Both"      if it is both (there are none < 100, but handle it)
@@ -20,6 +19,40 @@ public class Main {
         // Prime
         // Neither
         // Perfect
+         for (int i = 0; i < n; i++) {
+    int x = sc.nextInt();
+
+    // Check Prime
+    boolean isPrime = true;
+    if (x <= 1) isPrime = false;
+    for (int j = 2; j * j <= x; j++) {
+        if (x % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    // Check Perfect
+    int sum = 0;
+    for (int j = 1; j <= x / 2; j++) {
+        if (x % j == 0) {
+            sum += j;
+        }
+    }
+    boolean isPerfect = (sum == x && x != 0);
+
+    // Output
+    if (isPrime && isPerfect) {
+        System.out.println("Both");
+    } else if (isPrime) {
+        System.out.println("Prime");
+    } else if (isPerfect) {
+        System.out.println("Perfect");
+    } else {
+        System.out.println("Neither");
+    }
+}
+       
 
     }
 }
