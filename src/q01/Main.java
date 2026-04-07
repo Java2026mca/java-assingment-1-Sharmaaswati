730b53e65ff0af8ff897b70b8dc43ab329a34dcb
Sharmaaswati <sharma2807swati@gmail.com>
2026-04-07 17:18:32 +0530

    Implement diamond shape pattern printing
    Added code to print a diamond shape pattern using asterisks.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -20,6 +20,46 @@ public class Main {
         //  *   *
         //   * *
         //    *
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                System.out.print("*");
+
+                // inner spaces
+                for (int s = 1; s <= 2 * i - 3; s++) {
+                    System.out.print(" ");
+                }
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                System.out.print("*");
+                for (int s = 1; s <= 2 * i - 3; s++) {
+                    System.out.print(" ");
+                }
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
 
     }
 }
